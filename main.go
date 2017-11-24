00a2eccaa2da8a65531df87fceaeabd5431af32c
CheneyLew <cheneylew@163.com>
2017-11-24 22:09:17 +0800

    test

--- main.go
+++ main.go
@@ -12,14 +12,14 @@ import (
 func main() {
 	if false {
 		beego.Run()
+
+		fmt.Println(utils.SelfPath())
+		fmt.Println(colors.BlueBold("this is you name!"))
 	}
 
 
+	str := utils.FileReadAllString("/Users/apple/Desktop/a.txt")
+	utils.JJKPrintln(str)
 
-	fmt.Println(utils.SelfPath())
-	fmt.Println(colors.BlueBold("this is you name!"))
-	// fmt.Println(MY_TPL)
-
-	// ioutil.WriteFile("/Users/apple/Desktop/aa.m",[]byte(MY_TPL),0644)
 }
 
